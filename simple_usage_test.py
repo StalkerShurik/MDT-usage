00dd5e17a45fa026d00fc7b3ced6c725b81b975e
Alexander <agaponov04@gmail.com>
2023-05-20 11:59:09 +0300

    more tests

--- simple_usage_test.py
+++ simple_usage_test.py
@@ -10,9 +10,16 @@ img = cv.imread("DOG.jpg") #read image
 
 ret, img = cv.threshold(img, 127, 255, 0) # pixels > 127 becomes 255
 
+# img = np.where(img == 255, 2, img)
+# img = np.where(img == 0, 255, img)
+# img = np.where(img == 2, 0, img)
+
+# Image.fromarray(img).show()
+
 img = np.where(img == 255, 1, img) # all 255 becomes 1
 
-trans = np.array([[1, 0], [0, 1]]) #matrix induces Euclidean metric (you can use any positive-definite matrix)
+
+trans = np.array([[0.5, 0], [0, 20]]) #matrix induces Euclidean metric (you can use any positive-definite matrix)
 
 #params: image, transformation matrix, connectivity_type, is_signed
 #connectivity_type: 8-connectivity and 4-connectivity for 2d images, 26-connectivity and 6-connectivity for 3d
